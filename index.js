0d0cdb2145af708c8cfbf7d4a3918e934fdf18b6
Alicia Musili <aliciamusili2410@gmail.com>
2024-11-29 16:54:09 +0300

    project setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const doctors = require("./sample"); /// simulatin data fro a database
+
+const app = express(); // creating an express app
+// define our routes/different URIs
+app.get("/", (req, res) => {
+  // localhost:3003
+  res.render("index.ejs");
+  res.render("index.ejs", { doctors: doctors.slice(0, 6) });
+});
+app.get("/contact", (req, res) => {
+  // localhost:3003/contact
+  res.render("contact.ejs");
+});
+app.get("/doctors", (req, res) => {
+  // // localhost:3003/doctors
+  res.render("doctors.ejs");
+  res.render("doctors.ejs", { doctors: doctors });
+});
+//Start the app
+app.listen(3003, () => console.log("app running on port 3003"));
